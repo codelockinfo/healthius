bc6f1cd6b6e9beafb0ae91f8a20831a334880626
codelockinfo <codelockinfo@gmail.com>
2023-12-29 16:27:55 +0530

    discount 25 to 10

--- assets/custom-bundle.js
+++ assets/custom-bundle.js
@@ -608,7 +608,7 @@ $(document).ready(function() {
 		var affiliate_cookie = getCookie("discount_code");
 		var affiliate_cookie_backup = getCookie("50_Off_Discount");
 		var affuser_discounts = ['julian50', 'cpt50','claire50', 'hannah15', 'hanjam15', 'ashley15', 'kendra15', 'steve15', 'ryan15', 'ainsley15','dailypump50','save50'];
-		$discount_amount_price = 0.25;
+		$discount_amount_price = 0.10;
 		if((affiliate_cookie_backup != undefined) || (affiliate_cookie != undefined && affiliate_cookie != '')){
 			if(affiliate_cookie != undefined){
 				affiliate_cookie = affiliate_cookie.toLowerCase();
@@ -619,18 +619,6 @@ $(document).ready(function() {
 				$rightArrow = '<svg xmlns="http://www.w3.org/2000/svg" width="17.627" height="17.627" viewBox="0 0 17.627 17.627" aria-hidden="true" role="presentation" class="svgcolor w-3 h-3 mr-1 text-green-400"><g transform="translate(-16.457 -24.531)"><circle id="Ellipse_46" data-name="Ellipse 46" cx="7.5" cy="7.5" r="7.5" transform="translate(17.788 26.154)" fill="#fff"></circle><path id="noun_tick_684585" d="M15.413 6.6a8.813 8.813.0 1 0 8.813 8.813A8.805 8.805.0 0 0 15.413 6.6zm4.265 6.986-5.219 5.2a.8.8.0 0 1-.569.244.768.768.0 0 1-.569-.244l-2.579-2.559A.818.818.0 1 1 11.9 15.068l1.99 1.99 4.63-4.63a.809.809.0 0 1 1.158.0A.847.847.0 0 1 19.678 13.586z" transform="translate(9.857 17.931)" fill="currentColor"></path></g></svg>';
 				$('.25_savetext').html($rightArrow +"50% Off First Order");
 				$('.box-header-title').html('<div class="box-header-title">SUBSCRIBERS SAVE 50%<div><span class="subcarttitle">Applied at checkout</span></div></div>');
-				// $('.box-header-title').html('<div class="box-header-title"> <div><span class="subcarttitle">Free Meat of the Month with</span></div><div><span class="subcarttitle">Every Recurring Order!</span></div></div>');
-				// $('.announcement').text('EXCLUSIVE SALE: 6 FREE HOLIDAY MEATS');
-				if ($(window).width() > 700) {
-					free_pro_img = 'https://res.cloudinary.com/meals/image/upload/v1701388042/Cranapple_Bundler_Image.jpg';
-				}else{
-					free_pro_img = 'https://cdn.shopify.com/s/files/1/0555/1751/1961/files/Cranapple_Bundler_Image_mobile.jpg?v=1703781412';
-				}
-				free_pro_title = 'Free Order Gift';
-				var productBadge = "FREE";
-				bgcolor ="bg-green";
-				promo_class = 'promo-product';
-				turkey_product_padding = "turkey_product_padding";
 			}
 		}
 		console.log(free_pro_img + ".....free_pro_img");
