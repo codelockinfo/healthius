ec18e9d52c064bda94f943f998498b79bb4384ca
codelockinfo <codelockinfo@gmail.com>
2024-01-03 11:16:27 +0530

    freemeat 125 product image set variant wise &  desktop variant selection add to cart issue fixing

--- assets/custom-bundle.js
+++ assets/custom-bundle.js
@@ -429,7 +429,11 @@ $(document).ready(function() {
 	// TO DO NEED TO REMOVE
 	
 	function subscriptionAddtocart(){
-		$giftVariantid = $(".product-variant-select").val();
+		if ($(window).width() > 700) {
+			$giftVariantid = $(".maingiftproductitem .product-variant-select").val();
+		}else{
+			$giftVariantid = $(".box-giftproduct_mobile .product-variant-select").val();
+		}
 		$giftProductid = $(".giftProduct").data("product");
 		var PRODUCT_ID = $(".product_variant_id").val();
         console.log('giftVariantid');
